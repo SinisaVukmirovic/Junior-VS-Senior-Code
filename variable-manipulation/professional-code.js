04b2a6fe95697af579a98f2cfd408de8d3f8e944
Sinisa Vukmirovic <sinisavukmirovic@gmail.com>
2021-03-19 23:29:15 +0100

    Added professional type of code

--- variable-manipulation/professional-code.js
+++ variable-manipulation/professional-code.js
@@ -2,33 +2,33 @@
 
 // =====================================================
 
-function calculateTotal(items, options) {
-    // common beginner mistake is not setting dfault value for options
-    let t = 0;
-    items.forEach(i => {
-      t += i.price * i.quan;
-    });
-    t = t - t * (options.dis || 0);
-    t = t * 1.1;
-    // common beginner mistage with using ||,
-    // if something evaluates to 0, 0 || something will always be something
-    t = t + (options.ship || 5);
-    return t;
+const TAX_RATE = 1.1;
+const SHIPPING_DEFAULT = 5;
+
+// destructuring inside function arguments | also, using setting defaults so we don't have to worry about if checks
+function calculateTotal(items, { shipping = SHIPPING_DEFAULT, discount = 0 } = {}) {
+  if (items == null || items.length === 0) return 0;
+
+//   using reduce function instead of looping with a forEach | lso, using constant variable and not constantly updating variables and having helper intermediate calculations/variables  
+  const itemCost = items.reduce((total, item) => {
+    return total + item.price * item.quantity;
+  }, 0);
+  const discountRate = 1 - discount;
+
+  return itemCost * discountRate * TAX_RATE + shipping;
 }
-  
+
 const testItems = [
-    { price: 15, quan: 2 },
-    { price: 20, quan: 1 },
-    { price: 5, quan: 4 }
+  { price: 15, quantity: 2 },
+  { price: 20, quantity: 1 },
+  { price: 5, quantity: 4 }
 ]
 
-
-  
-  // console.log(calculateTotal())
-  // console.log(calculateTotal(testItems))
-  // console.log(calculateTotal(undefined, {}))
-  console.log(calculateTotal([], {}));
-  console.log(calculateTotal(testItems, {}));
-  console.log(calculateTotal(testItems, { ship: 0 }));
-  console.log(calculateTotal(testItems, { dis: .75 }));
-  console.log(calculateTotal(testItems, { ship: 12 }));
+// console.log(calculateTotal());
+// console.log(calculateTotal(testItems));
+// console.log(calculateTotal(undefined, {}));
+// console.log(calculateTotal([], {}));
+console.log(calculateTotal(testItems, {}));
+console.log(calculateTotal(testItems, { shipping: 0 }));
+console.log(calculateTotal(testItems, { discount: .75 }));
+console.log(calculateTotal(testItems, { shipping: 12 }));
